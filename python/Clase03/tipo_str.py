b9a687b08fd888b95cfee2e23f16d0e1c7e908be
MarcelaMolina <marcelamolina726@gmail.com>
2024-10-04 10:45:17 -0300

    Clase02y03

---
+++ python/Clase03/tipo_str.py
@@ -0,0 +1,12 @@
+import math
+
+#Profundizamos el tipo String
+#Concatenación automática en python
+
+variable = ' Adiós'
+mensaje = 'Hola ''Alumnos' + variable
+mensaje += ', Terminamos'
+#print(mensaje)
+
+#Usamos la clase help para ayuda o documentación (built-in)
+help(math.isnan)
